3f42dabb486ae814e8a5770d39674498b3b8819b
LYF <863978071@qq.com>
2021-06-25 18:35:07 +0800

    first

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/liu18373639121/go_testMath
+
+go 1.16
---
+++ math.go
@@ -0,0 +1,8 @@
+package go_testMath
+
+import "fmt"
+
+func Add(a int, b int) int {
+	fmt.Println("this is form github Add")
+	return a + b
+}
